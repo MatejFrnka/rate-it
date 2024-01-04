9c79459245c122e028796dfad9eaaad092d416e9
Vladislav Chval <vladislavchval@gmail.com>
2024-01-04 16:05:33 +0100

    RIB-33-Implemented horizontal scrolling

---
+++ src/main/resources/static/scripts/slider-script.js
@@ -0,0 +1,28 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const glide = new Glide(".glide", {
+//         type: "carousel",
+//         startAt: 0,
+//         perView: 4,
+//         focusAt: 0,
+//         spaceBetween: 10,
+//         bound: true,
+//         peek: {
+//             before: 0,
+//             after: 1,
+//         },
+//         breakpoints: {
+//             350: {
+//                 perView: 1,
+//             },
+//             700: {
+//                 perView: 2,
+//             },
+//             900: {
+//                 perView: 2,
+//             },
+//             1200: {
+//                 perView: 3,
+//             },
+//         },
+//     }).mount();
+// });
