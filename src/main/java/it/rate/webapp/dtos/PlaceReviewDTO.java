74c37e153db40910571905ce7de6e9a4d2df4fd8
Martin Hübner <marthubner@gmail.com>
2024-02-06 18:11:10 +0100

    RIB-186: add link to users profile to place page

--- src/main/java/it/rate/webapp/dtos/PlaceReviewDTO.java
+++ src/main/java/it/rate/webapp/dtos/PlaceReviewDTO.java
@@ -1,5 +1,7 @@
 package it.rate.webapp.dtos;
 
+import it.rate.webapp.models.AppUser;
+import it.rate.webapp.models.Place;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -10,4 +12,22 @@ public record PlaceReviewDTO(
     String review,
     List<RatingDTO> ratings,
     Double avgRating,
-    Timestamp timestamp) {}
+    Timestamp timestamp) {
+
+  public PlaceReviewDTO(
+      AppUser appUser,
+      Place place,
+      String review,
+      List<RatingDTO> ratings,
+      Double avgRating,
+      Timestamp timestamp) {
+    this(
+        appUser.getUsername(),
+        place.getName(),
+        place.getId(),
+        review,
+        ratings,
+        avgRating,
+        timestamp);
+  }
+}
