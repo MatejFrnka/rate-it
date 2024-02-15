7802bff984492d5138ad9012dea66096242294c8
Martin Hübner <marthubner@gmail.com>
2024-02-15 14:35:41 +0100

    RIB-197 fix style

--- src/main/java/it/rate/webapp/dtos/CategoryDTO.java
+++ src/main/java/it/rate/webapp/dtos/CategoryDTO.java
@@ -3,7 +3,7 @@ package it.rate.webapp.dtos;
 import it.rate.webapp.models.Category;
 
 public record CategoryDTO(Long id, String name) {
-    public CategoryDTO(Category category) {
-        this(category.getId(), category.getName());
-    }
+  public CategoryDTO(Category category) {
+    this(category.getId(), category.getName());
+  }
 }
